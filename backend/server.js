21f74f7fb4ee17639759f279913b2a7fe6f8f73c
marinacavar <marinacavar2@gmail.com>
2024-08-22 17:11:59 +0200

    Postavljanje backend dijela aplikacije

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+//require('./config/db');
+//require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('express').json;
+const cookieParser = require('cookie-parser');
+//const cronJobs = require('./cron');
+
+const app = express();
+const port = 3001;
+
+//app.use(cookieParser());
+//app.use(bodyParser());
+//app.use(cors())
+//app.use('/',require('./routes/router'))
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
+
